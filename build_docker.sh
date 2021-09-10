0c8fd34b12a26b1ff3266ebd610dae37766a1821
Semyon.Atamas <semyon.atamas@jetbrains.com>
2021-09-10 20:20:19 +0300

    Add kubernetes config

--- build_docker.sh
+++ build_docker.sh
@@ -2,6 +2,6 @@
 
 set -e
 IMAGE_TAG=$1
-IMAGE=registry.jetbrains.team/mau/crowdin-grazie:${IMAGE_TAG:-latest}
+IMAGE=registry.jetbrains.team/p/mau/crowdin-integrations/crowdin-grazie:${IMAGE_TAG:-latest}
 docker build . --tag $IMAGE
 docker push $IMAGE
