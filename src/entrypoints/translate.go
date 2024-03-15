6f948dc794adab48fe9c2ddb60fdb64bf2987866
Andrey Vasin <andrey.vasin@jetbrains.com>
2024-03-15 19:16:17 +0100

    MAU-961 * Use Task API in parallel

--- src/entrypoints/translate.go
+++ src/entrypoints/translate.go
@@ -13,10 +13,6 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-const (
-	batchSize = 4
-)
-
 type request struct {
 	Strings []string `json:"strings"`
 }
@@ -86,48 +82,19 @@ func (hc *HandlerCreator) TranslateHandler(grazieMlClient graziego.Client, clien
 
 		target := query.Get("target")
 
-		results := make(map[int64][]string, len(requestBody.Strings)/batchSize)
+		results := make(map[int]string, len(requestBody.Strings))
 		var reqError error
 		wg := sync.WaitGroup{}
 		m := sync.Mutex{}
-		number := int64(0)
 
-		for i := 0; i < len(requestBody.Strings); i += batchSize {
-			end := i + batchSize
-			if end > len(requestBody.Strings) {
-				end = len(requestBody.Strings)
-			}
-
-			batch := requestBody.Strings[i:end]
-
+		for i := 0; i < len(requestBody.Strings); i += 1 {
 			wg.Add(1)
-			go func(number int64, batch []string) {
+			go func(number int, stringToTranslate string) {
 				defer wg.Done()
 
-				data, err := json.Marshal(batch)
-				if err != nil {
-					logEntry.WithField("strings", batch).Error("failed to marshal strings")
-
-					m.Lock()
-					defer m.Unlock()
-					reqError = fmt.Errorf("failed to marshal strings: %v", err)
-					return
-				}
-
-				prompt := fmt.Sprintf(
-					"Translate JSON array of strings to the target language. "+
-						"Answer with only JSON array of translated strings in the same order. Do not use any wrapping for response.\n"+
-						"Target language code: %s\n"+
-						"Array:\n%s",
-					target, string(data),
+				translateResponse, err := grazieMlClient.Translate(
+					r.Context(), graziego.CrowdinTranslateTag, target, stringToTranslate,
 				)
-
-				chatgptResp, err := grazieMlClient.Chat(r.Context(), "gpt-4-1106-preview", []graziego.ChatMessage{
-					{
-						Role: graziego.RoleUser,
-						Text: prompt,
-					},
-				})
 				if err != nil {
 					logEntry.WithError(err).Error("error translating")
 
@@ -137,26 +104,10 @@ func (hc *HandlerCreator) TranslateHandler(grazieMlClient graziego.Client, clien
 					return
 				}
 
-				answer := chatgptResp.Text
-				logEntry = logEntry.WithField("answer", answer)
-
-				batchTranslations := make([]string, 0)
-				err = json.Unmarshal([]byte(answer), &batchTranslations)
-				if err != nil {
-					logEntry.WithError(err).Error("failed to unmarshal response")
-
-					m.Lock()
-					defer m.Unlock()
-					reqError = fmt.Errorf("failed to unmarshal response: %v", err)
-					return
-				}
-
 				m.Lock()
 				defer m.Unlock()
-				results[number] = batchTranslations
-			}(number, batch)
-
-			number += 1
+				results[number] = translateResponse
+			}(i, requestBody.Strings[i])
 		}
 
 		wg.Wait()
@@ -167,10 +118,8 @@ func (hc *HandlerCreator) TranslateHandler(grazieMlClient graziego.Client, clien
 		}
 
 		translations := make([]string, 0, len(requestBody.Strings))
-		number = 0
-		for i := 0; i < len(requestBody.Strings); i += batchSize {
-			translations = append(translations, results[number]...)
-			number += 1
+		for i := 0; i < len(requestBody.Strings); i += 1 {
+			translations = append(translations, results[i])
 		}
 
 		resp := response{
